e658c5e873e0133437d1bb179c1c4aeb7c8da674
Matej Vavrinec <wmatex@gmail.com>
2024-11-23 23:08:58 +0100

    Use cobra & viper for config management

---
+++ cmd/root.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"fmt"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+)
+
+const APP_NAME = "automux"
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   APP_NAME,
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(viper.GetStringSlice("projects.lookup_dirs"))
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $XDG_CONFIG_HOME/automux/config.toml)")
+}
+
+func initConfig() {
+	// Don't forget to read config either from cfgFile or from home directory!
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		xdgConfigHome := os.Getenv("XDG_CONFIG_HOME")
+		if xdgConfigHome == "" {
+			home, err := homedir.Dir()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			xdgConfigHome = filepath.Join(home, ".config")
+		}
+		configPath := filepath.Join(xdgConfigHome, APP_NAME)
+
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(configPath)
+		viper.SetConfigType("toml")
+		viper.SetConfigName("config.toml")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Can't read config:", err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/wmatex/automux/internal/config"
-)
+import "github.com/wmatex/automux/cmd"
 
 func main() {
-	var config config.Config
-	err := config.Load()
-	if err != nil {
-		log.Fatalf("Error loading config: %s\n", err)
-	}
-	fmt.Println(config)
-	fmt.Println("Hello world")
+	cmd.Execute()
 }
